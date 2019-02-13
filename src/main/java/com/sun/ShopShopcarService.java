9df2f19c0f2faef5c70d9b9b59e0e1b780c92d0a
suxin <371675404@qq.com>
2019-02-13 14:33:27 +0800

    初始化

---
+++ src/main/java/com/sun/ShopShopcarService.java
@@ -0,0 +1,14 @@
+package com.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopShopcarService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopShopcarService.class, args);
+    }
+
+}
+
